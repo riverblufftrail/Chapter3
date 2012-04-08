81e1c7b244446ea94e14eb66c5e7536c0b3ac189
Brian Ricker <brian.ricker@g5searchmarketing.com>
2012-04-08 13:40:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Less::Application.config.secret_token = 'bcf6d32dd61907ca345e34dea65591aeb801e699a98de1a92e59ba73c2b8941c0fc704baa51b9572ba56208c67d2fc71cf927873eb87e593dc6cd8cc86318e43'
